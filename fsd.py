58b5f7bf41562c24330a6cb95944945cd96e800e
miguelflor <cidflor2004@gmail.com>
2022-01-10 17:26:33 +0000

    yeye

--- fsd.py
+++ fsd.py
@@ -1,3 +1,11 @@
-x = 'zxczxcxz'
-x = '"'+str(x)+'"'
-print(x)
+import json 
+
+with open("json/dictionary.json") as dictt:
+    dictionary = json.load(dictt)
+while True:
+    word = input("pls what word do u want a defeniction:--->")
+    try:
+        deff = dictionary[word]
+    except:
+        deff  = "i don't know"
+    print(deff)
