a6665e5e23ba5d02ce4d971e7db0caa2b85c7ac2
miguelflor <cidflor2004@gmail.com>
2022-01-21 09:12:46 +0000

    tentar fazer coisas
    condifar

--- fsd.py
+++ fsd.py
@@ -1,8 +1,35 @@
 import json 
 
+def trust(dictionary):
+    p = 0
+    words = []
+
+    for x in dictionary:
+        for y in x:
+            for k in dictionary:
+                if k == y:
+                    if k not in words:
+                        words = words + [k]
+    
+    print(words)
+    with open("text.txt", "a") as txt:
+        txt.write(str(words))
+        txt.close()
+                        
+def trustt(dictionary):
+    for x in dictitonary:
+        print(x)
+
+
+
 with open("json/dictionary.json") as dictt:
     dictionary = json.load(dictt)
-p = 0
-words = []
+
+# trust(dictionary)
+trustt(dictionary)
+
+
+
+
    
 #ver se as palavras das defenições do dicionário estão no próprio dicionário se não irá guradalas num array e dar print;
