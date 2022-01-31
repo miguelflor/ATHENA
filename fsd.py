1c0c8c6292e3d4b8f39f45d5e0941deb6cbe4319
miguelflor <cidflor2004@gmail.com>
2022-01-31 17:30:42 +0000

    Update fsd.py

--- fsd.py
+++ fsd.py
@@ -1,17 +1,62 @@
 import json 
+import os
 
+from cv2 import BackgroundSubtractor
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+    
 
 
 with open("json/dictionary.json") as dictt:
     dictionary = json.load(dictt)
 
-word = input("what is the word that  seak defeniction")
-# trust(dictionary)
-#trust(dictionary)
-try:
-    print(dictionary[word])
-except:
-    print("asndaçsd")
+i = 0
+for x in dictionary:
+    i+=1
+
+print(i)
+
+barra = "[                    ]"
+
+#cada barra corresponde a 5%
+e = 0
+p = 1
+clearConsole()
+print(str(barra))
+for x in dictionary:
+    e+=1
+    
+    if e == 5110:
+        clearConsole()
+        barra = list(barra)
+        barra[p] ="|"
+        barra  ="".join(barra)
+
+        p+=1
+        print(str(barra))
+
+        e = 0
+    defi = dictionary[x]
+    new_defi = ""
+   
+
+    for y in defi:
+        if y != "1.":
+            new_defi = new_defi +y
+            if "." in list(y):
+                new_defi = new_defi +y
+                break
+        
+
+print(dictionary)
+    
+
+
+
 
 
 
